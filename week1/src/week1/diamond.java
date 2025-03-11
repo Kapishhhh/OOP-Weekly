5df65a8b34c9f71af110da374c66177b641b9431
kapishsah <kapishsah@gmail.com>
2025-03-11 20:24:52 +0545

    Finished

---
+++ week1/src/week1/diamond.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class diamond {
+
+	public static void main(String[] args) {
+
+		System.out.println("   *   ");
+		System.out.println("  ***  ");
+		System.out.println(" ***** ");
+		System.out.println("*******");
+		System.out.println(" ***** ");
+		System.out.println("  ***  ");
+		System.out.println("   *   ");
+
+		
+	}
+
+}
---
+++ week1/src/week1/personaldetails.java
@@ -0,0 +1,25 @@
+package week1;
+
+public class personaldetails {
+
+   public static void main(String[] args) {
+			
+	String name = "Kapish";
+	String address = "Balkumari";
+	int age = 20;
+	String number = "9844333505";
+	
+	System.out.println("Personal Details:- ");
+	System.out.println("");
+	System.out.println("Name: " + name);
+	System.out.println("Address: " + address);
+	System.out.println("Age: " + age);
+	System.out.println("Number: " + number);
+
+	
+	
+		}
+	}
+
+
+
